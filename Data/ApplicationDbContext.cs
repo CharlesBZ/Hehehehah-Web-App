ba2b3828dd78a493dd24d56a74d38c42fe0f6f70
Charles A. Best-Zambrana <charles.bestzambrana@gmail.com>
2025-03-30 19:40:16 -0400

    add jokes controller

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using HehehehahWebApp.Models;
+
+namespace HehehehahWebApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<HehehehahWebApp.Models.Joke> Joke { get; set; } = default!;
+    }
+}
